eb5e6dc87b880eab3d263deb891dfb2767e19b7c
Shivani <shivani.ratnaparkhi@vertisinfotech.com>
2015-07-27 16:19:49 +0530

    Simple cart created

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,16 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   scope :latest, -> { order(:updated_at).last }
+
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  def ensure_not_referenced_by_any_line_item
-    if line_items.empty?
-      return true
-    else
-      errors.add(:base, 'Line Items present')
-      return false
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
     end
-  end
 end
